b114bd99c65ab2177554e107c319b9ebd5509775
Shivshankar Ganapuram <ganapuramshiva07@gmail.com>
2023-12-28 12:17:58 +0530

    added more concept

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const menuItems = [
+    { id: 'linux', label: 'Linux', file: 'linux.html'},
     { id: 'containerization', label: "Containerization", file: 'containerization.html' },
     { id: 'orchestration', label: "Orchestration", file: 'orchestration.html' },
     { id: 'infrastructure-as-code', label: "Infrastructure as Code", file: 'infrastructure-as-code.html' },
     { id: 'configuration-management', label: "Configuration Management", file: 'configuration-management.html' },
+    { id: 'cloud', label: 'Cloud', file: 'cloud.html'},
+    { id: 'security-devops', label: 'Security DevOps', file: 'security-devops.html'}
 ];
 
 // Function to load menu dynamically
